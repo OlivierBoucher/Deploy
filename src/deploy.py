e97f8c21401d16d78816b383b46b1356bc5c1f97
Olivier Boucher <admin@olivierboucher.com>
2016-02-16 15:36:31 +0000

    drafting init method

--- src/deploy.py
+++ src/deploy.py
@@ -5,17 +5,26 @@ class Deploy(object):
         self.cmd = command
         self.config = config
         self.commands = {
-            'init': self.__init,
-            'now': self.__now,
+            'init': self.__cmd_init,
+            'now': self.__cmd_now,
         }
 
-    def __init(self):
-        print 'init'
+    def __cmd_init(self):
         # testing utilities
         test = get_string('Enter your name', default='Olivier', validate=lambda x: True, error_msg='HH')
-        print 'you entered ' + test
+        
+        # Local info
+        #   Project name, default to current dir
+        #   Preset, must choose from available presets
+        #   Source dir
+        #   Build dir
+        
+        # Remote
+        #   Server address
+        #   Server user
+        
 
-    def __now(self):
+    def __cmd_now(self):
         print 'now'
 
     def execute(self):
